8b09be3734b47b0d8abdf43dcda1d42aec9e1699
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-11-28 21:16:35 +0200

    Add signal for creating a profile, when creating user

--- GiftWithCause_DjangoProject/accounts/apps.py
+++ GiftWithCause_DjangoProject/accounts/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'GiftWithCause_DjangoProject.accounts'
+
+    def ready(self):
+        import GiftWithCause_DjangoProject.accounts.signals
