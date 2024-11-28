dc41eb51b2ec29fd510e8d701a23c3bb74724bd7
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-11-28 21:17:42 +0200

    Update accounts urls, adding paths login, register, logout, profile

--- GiftWithCause_DjangoProject/accounts/urls.py
+++ GiftWithCause_DjangoProject/accounts/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.contrib.auth.views import LogoutView
+from django.urls import path, include
 
 from GiftWithCause_DjangoProject.accounts import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name="home"),
+    path('login/', views.AppUserLoginView.as_view(), name='login'),
+    path('register/', views.AppUserRegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/<int:pk>/', include([
+        path('', views.ProfileDetailView.as_view(), name='profile-details'),
+        path('edit/', views.ProfileEditView.as_view(), name='profile-edit'),
+        path('delete/', views.ProfileDeleteView.as_view(), name='profile-delete'),
+    ]))
 ]
