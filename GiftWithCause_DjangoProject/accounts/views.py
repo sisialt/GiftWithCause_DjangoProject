289b7b97b28b43c9cd18e8130b222b25383344b5
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-12-01 15:51:37 +0200

    Add login/delete/details/edit views

--- GiftWithCause_DjangoProject/accounts/views.py
+++ GiftWithCause_DjangoProject/accounts/views.py
@@ -1,9 +1,73 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.contrib.auth import get_user_model, login
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.views import LoginView
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, UpdateView
 
+from GiftWithCause_DjangoProject.accounts.forms import AppUserCreationForm, ProfileEditForm
 from GiftWithCause_DjangoProject.accounts.models import Profile
 
+UserModel = get_user_model()
 
-class HomeView(ListView):
+
+class AppUserLoginView(LoginView):
+    template_name = 'login.html'
+
+
+class AppUserRegisterView(CreateView):
+    model = UserModel
+    form_class = AppUserCreationForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        login(self.request, self.object)
+
+        return response
+
+
+class ProfileDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Profile
-    template_name = 'my-index.html'
+    template_name = 'profile-delete.html'
+    success_url = reverse_lazy('login')
+
+    def test_func(self):
+        profile = get_object_or_404(Profile, pk=self.kwargs['pk'])
+        return self.request.user == profile.user
+
+
+class ProfileDetailView(LoginRequiredMixin, DetailView):
+    model = UserModel
+    template_name = 'profile-details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # photos_with_likes = self.object.photo_set.annotate(likes_count=Count('like'))
+        #
+        # context['total_likes_count'] = sum(photo.likes_count for photo in photos_with_likes)
+        # context['total_pets_count'] = self.object.pet_set.count()
+        # context['total_photos_count'] = self.object.photo_set.count()
+
+        return context
+
+
+class ProfileEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Profile
+    form_class = ProfileEditForm
+    template_name = 'profile-edit.html'
+
+    def test_func(self):
+        profile = get_object_or_404(Profile, pk=self.kwargs['pk'])
+        return self.request.user == profile.user
+
+    def get_success_url(self):
+        return reverse_lazy(
+            'profile-details',
+            kwargs={
+                'pk': self.object.pk,
+            }
+        )
