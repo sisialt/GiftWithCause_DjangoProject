15a60516dedb84b38db6a3544bd907e0c60f6762
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-12-04 22:24:25 +0200

    Update signal to create an instance of GiftCreator if the user is creator

--- GiftWithCause_DjangoProject/accounts/signals.py
+++ GiftWithCause_DjangoProject/accounts/signals.py
@@ -2,11 +2,16 @@ from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from GiftWithCause_DjangoProject.accounts.models import Profile
+from GiftWithCause_DjangoProject.gift_creators.models import GiftCreator
 
 UserModel = get_user_model()
 
 
 @receiver(post_save, sender=UserModel)
-def create_profile(sender, instance, created, **kwargs):
+def create_profile_and_creator(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+        if instance.is_creator:
+            GiftCreator.objects.create(user=instance)
+
