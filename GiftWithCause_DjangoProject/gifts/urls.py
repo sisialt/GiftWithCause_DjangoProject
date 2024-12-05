a9f8703f4bb0f7061ee1ba5ac32aa6d6be959314
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-12-05 22:01:51 +0200

    Create paths for gift create/details/edit/delete

--- GiftWithCause_DjangoProject/gifts/urls.py
+++ GiftWithCause_DjangoProject/gifts/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 
 from GiftWithCause_DjangoProject.gifts import views
 
 urlpatterns = [
     path('', views.GiftOffersView.as_view(), name="gift-offers"),
+    path('create/', views.GiftCreateView.as_view(), name='gift-create'),
+    path('<int:pk>/', include([
+        path('details/', views.GiftDetailView.as_view(), name="gift-details"),
+        path('edit/', views.GiftEditView.as_view(), name="gift-edit"),
+        path('delete/', views.GiftDeleteView.as_view(), name="gift-delete"),
+    ]))
 ]
