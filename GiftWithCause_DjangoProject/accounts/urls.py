5d279a52dacebc5d938b131e8b2e89ae4b52b126
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-12-06 21:39:01 +0200

    Change profile details url

--- GiftWithCause_DjangoProject/accounts/urls.py
+++ GiftWithCause_DjangoProject/accounts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('register/', views.AppUserRegisterView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/<int:pk>/', include([
-        path('', views.ProfileDetailView.as_view(), name='profile-details'),
+        path('details/', views.ProfileDetailView.as_view(), name='profile-details'),
         path('edit/', views.ProfileEditView.as_view(), name='profile-edit'),
         path('delete/', views.AppUserDeleteView.as_view(), name='profile-delete'),
         path('published-gifts/', views.ProfilePublishedGiftsView.as_view(), name='published-gifts'),
