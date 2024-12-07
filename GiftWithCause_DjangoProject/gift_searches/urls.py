5cf860bc9973df393d6d029857dd0d7cd53a739f
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2024-12-07 21:18:47 +0200

    Add gift_searches paths (create, details, edit, delete)

--- GiftWithCause_DjangoProject/gift_searches/urls.py
+++ GiftWithCause_DjangoProject/gift_searches/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from GiftWithCause_DjangoProject.gift_searches import views
 
 urlpatterns = [
     path('', views.GiftSearchesView.as_view(), name="gift-searches"),
+    path('create/', views.GiftSearchCreateView.as_view(), name='gift-search-create'),
+    path('<int:pk>/', include([
+        path('details/', views.GiftSearchDetailView.as_view(), name="gift-search-details"),
+        path('edit/', views.GiftSearchEditView.as_view(), name="gift-search-edit"),
+        path('delete/', views.GiftSearchDeleteView.as_view(), name="gift-search-delete"),
+    ]))
 ]
